b4df2ec24d0d5bd4571c950e97394b2737969568
yann <yann@wikiglobal.com>
2024-06-24 11:10:23 +0800

    feat: 国家对应区域码更新

---
+++ country/country_test.go
@@ -0,0 +1,20 @@
+package country
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetAreaCodeByCode(t *testing.T) {
+	fmt.Println("二字码数量：", len(twoCharCodeToCountryCodeMapping))
+	fmt.Println("三字码数量：", len(threeCharCodeToCountryCodeMapping))
+	fmt.Println("国家总数量：", len(countryCodeMapping))
+	fmt.Println("国家区域总数量：", len(countryAreaMapping))
+	for key, _ := range countryCodeMapping {
+		areaCode, ok := countryAreaMapping[key]
+		if !ok || areaCode == "" {
+			fmt.Printf("国家：%v 不存在\n", key)
+			continue
+		}
+	}
+}
