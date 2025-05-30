7167a6dcb96bcfbb9918659e6012c19fb8ade66f
yann <yann@wikiglobal.com>
2025-05-30 18:14:27 +0800

    feat: 添加新错误

--- errors/errors.go
+++ errors/errors.go
@@ -6,11 +6,12 @@ import (
 
 var (
 	ErrBadRequest          = errors.New(400, "INVALID_ARGS", "INVALID ARGS")
-	ErrLogin               = errors.New(401, "UNAUTHORIZED", "请授权登录")
-	ErrRefreshTokenExpired = errors.New(400, "REFRESH_TOKEN_EXPIRED", "请重新登录")
-	ErrAccessTokenExpired  = errors.New(400, "ACCESS_TOKEN_EXPIRED", "token已经过期")
-	ErrResourceNotFound    = errors.New(404, "RESOURCE_NOT_FOUND", "资源不存在")
-	ErrInternalServer      = errors.New(500, "INTERNAL_SERVER_ERROR", "服务内部错误")
+	ErrRefreshTokenExpired = errors.New(400, "REFRESH_TOKEN_EXPIRED", "not auth")
+	ErrAccessTokenExpired  = errors.New(400, "ACCESS_TOKEN_EXPIRED", "token expired")
+	ErrLogin               = errors.New(401, "UNAUTHORIZED", "not auth")
+	ErrUserOperation       = errors.New(402, "USER_OPERATION", "try again later")
+	ErrResourceNotFound    = errors.New(404, "RESOURCE_NOT_FOUND", "resource not found")
+	ErrInternalServer      = errors.New(500, "INTERNAL_SERVER_ERROR", "internal server err")
 )
 
 func WithReason(e *errors.Error, in string) *errors.Error {
